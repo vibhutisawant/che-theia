9d7b6f8ebe7a3de424a535d7335742a0280d3dae
vibhutisawant <Vibhuti.Sawant@ibm.com>
2021-05-12 14:04:45 +0530

    update

--- make-release.sh
+++ make-release.sh
@@ -12,9 +12,6 @@
 
 NOCOMMIT=0
 
-git remote remove origin
-git remote add origin https://${CHE_BOT_GITHUB_TOKEN}@github.com/vibhutisawant/che-theia.git
-
 while [[ "$#" -gt 0 ]]; do
   case $1 in
     '-v'|'--version') VERSION="$2"; shift 1;;
