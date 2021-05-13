9ed49fd012bfa05ec9166b0f006f7ee102f044bc
vibhutisawant <Vibhuti.Sawant@ibm.com>
2021-05-13 16:41:36 +0530

    linting fix

--- .travis/docker_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-platform=$1
-
-docker image prune -a -f
-docker pull quay.io/prabhav/che-theia-dev:next
-docker tag quay.io/prabhav/che-theia-dev:next prabhav/che-theia-dev:next
-./build.sh --root-yarn-opts:--ignore-scripts --dockerfile:Dockerfile.${platform}
--- .travis/publish_multiarch.sh
+++ .travis/publish_multiarch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-TAG=$1
 REGISTRY="quay.io"
 
 PUBLISH_IMAGES_LIST=(
